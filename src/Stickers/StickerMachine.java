4ca4afd0e8f218ee0a5012f044610ee710b232db
Anderson José d.Luz Júnior <Anderson_Junior2@hotmail.com>
2023-03-30 16:06:41 -0300

    criando mais detalhes

--- src/Stickers/StickerMachine.java
+++ src/Stickers/StickerMachine.java
@@ -1,14 +1,18 @@
 package Stickers;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 
 import javax.imageio.ImageIO;
 
@@ -16,29 +20,55 @@ public class StickerMachine {
 
 	public void create(InputStream input, String nomeArquivo) throws IOException {
 		// leitura da imagem
-		//InputStream input = new FileInputStream(new File("entrada/Cachorro_loco.jpg"));
-		//InputStream inputURL = new URL("https://www.qualittas.com.br/blog/wp-content/uploads/2017/08/M%C3%AAs-do-cachorro-louco.png").openStream();
+		// InputStream input = new FileInputStream(new
+		// File("entrada/Cachorro_loco.jpg"));
+		// InputStream inputURL = new
+		// URL("https://www.qualittas.com.br/blog/wp-content/uploads/2017/08/M%C3%AAs-do-cachorro-louco.png").openStream();
 		BufferedImage image = ImageIO.read(input);
-		
+
 		// criar uma nova imagem em memória e com transparência
 		int width = image.getWidth();
 		int height = image.getHeight();
 		int newHeight = height + 200;
-		
+
 		BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
 		// copiar imagem original pra uma nova imagem (em memória)
 		Graphics2D graphic = (Graphics2D) newImage.getGraphics();
 		graphic.drawImage(image, null, 0, 0);
-		
+
 		// escrever uma frase na nova imagem
-		var style = new Font(Font.SANS_SERIF, Font.BOLD, 65);
+		var style = new Font("Impact", Font.BOLD, 65);
 		graphic.setFont(style);
 		graphic.setColor(Color.black);
+
+		String text = "FILMASSO";
+		FontMetrics fontMetric = graphic.getFontMetrics();
+		Rectangle2D ret = fontMetric.getStringBounds(text, graphic);
+
+		int largText = (int) ret.getWidth();
+		int positionTextX = (width - largText) / 2;
+		int positionTextY = newHeight - 100;
+
+		graphic.drawString(text, positionTextX, positionTextY);
 		
-		graphic.drawString("FILMASSO", 200, newHeight - 100);
+		var fontRender = graphic.getFontRenderContext();
+		var textLayout = new TextLayout(text, style, fontRender);
 		
+		Shape outline = textLayout.getOutline(null);
+		AffineTransform transform = graphic.getTransform();
+		
+		transform.translate(positionTextX, positionTextY);
+		graphic.setTransform(transform);
+		
+		var outlineStroke = new BasicStroke(width * 0.004f);
+		graphic.setStroke(outlineStroke);
+		
+		graphic.setColor(Color.magenta);
+		graphic.draw(outline);
+		graphic.setClip(outline);
+
 		// escrever a nova imagem em arquivo
-		ImageIO.write(newImage, "png", new File("saida/" + nomeArquivo));
-		
+		ImageIO.write(newImage, "png", new File(nomeArquivo));
+
 	}
 }
--- src/Systeminic/PrimeiroTeste.java
+++ src/Systeminic/PrimeiroTeste.java
@@ -2,6 +2,8 @@ package Systeminic;
 
 import JsonParser.*;
 import Stickers.StickerMachine;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -16,8 +18,7 @@ import java.util.Map;
 public class PrimeiroTeste {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		// API do HTTP, get and request = buscar na api do google e devolver o body em
-		// forma de String
+		// API do HTTP, get and request = buscar na api do google e devolver o body em forma de String
 		String color = "\u001b[38;5;246m", re_color = "\u001b[m";
 		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 		URI endereco = URI.create(url);
@@ -33,11 +34,17 @@ public class PrimeiroTeste {
 
 		// exibição de dados
 		var sm = new StickerMachine();
+		var diretorio = new File("saida/");
 		
-		for (Map<String, String> filme : listaDeFilmes) {
+		diretorio.mkdir();
+		
+		for (int i = 0; i < 5; i++) {
+			var filme = listaDeFilmes.get(i);
+		
 			String urlTitle = filme.get("title");
 			String urlImage = filme.get("image");
-			String nomeArq = urlTitle + ".png";
+			String urlRating = filme.get("imDbRating");
+			String nomeArq = "saida/" + urlTitle + ".png";
 			
 			InputStream inputURL = new URL(urlImage).openStream();
 			
@@ -45,10 +52,9 @@ public class PrimeiroTeste {
 
 			System.out.println(color + "Title: " + re_color + urlTitle);
 			System.out.println(color + "Poster: " + re_color + urlImage);
-			System.out.println(color + "Rating: " + re_color + filme.get("imDbRating"));
+			System.out.println(color + "Rating: " + re_color + urlRating);
 			System.out.println("");
 		}
-
 	}
 
 }
